a0d74d1072d8c09901c3459b2dab79c9bb5c19f5
sniffjoke <sniffjoke@gmail.com>
2024-07-28 17:25:16 +0200

    did push, pop, shift, unshift

---
+++ index.js
@@ -0,0 +1,92 @@
+// let fruits = ['apple', 'banana', 'orange'];
+//
+// let newFruits = fruits.push('peach')
+//
+// console.log(newFruits)
+// console.log(fruits)
+
+// const users = [
+//     {
+//         id: 1,
+//         name: 'Bob',
+//         isStudent: true,
+//     },
+//     {
+//         id: 2,
+//         name: 'Alex',
+//         isStudent: true,
+//     },
+//     {
+//         id: 3,
+//         name: 'Ann',
+//         isStudent: true,
+//     },
+//     {
+//         id: 4,
+//         name: 'Donald',
+//         isStudent: false,
+//     }
+// ]
+//
+// const lastUser = users.pop()
+//
+// console.log(lastUser)
+// console.log(users)
+
+// const users = [
+//     {
+//         id: 1,
+//         name: 'Bob',
+//         isStudent: true,
+//     },
+//     {
+//         id: 2,
+//         name: 'Alex',
+//         isStudent: true,
+//     },
+//     {
+//         id: 3,
+//         name: 'Ann',
+//         isStudent: true,
+//     },
+//     {
+//         id: 4,
+//         name: 'Donald',
+//         isStudent: false,
+//     },
+// ];
+//
+// const firstUser = users.shift()
+// console.log(firstUser)
+// console.log(users)
+
+// const users = [
+//     {
+//         id: 1,
+//         name: 'Bob',
+//         isStudent: true,
+//     },
+//     {
+//         id: 2,
+//         name: 'Alex',
+//         isStudent: true,
+//     },
+//     {
+//         id: 3,
+//         name: 'Ann',
+//         isStudent: true,
+//     },
+//     {
+//         id: 4,
+//         name: 'Donald',
+//         isStudent: false,
+//     },
+// ];
+//
+// users.unshift({
+//     id: 0,
+//     name: 'TestName',
+//     isStudent: false,
+// },)
+//
+// console.log(users)
