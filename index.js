7b8889b35462b7551ec212cc18ddfb95a07e1da2
sniffjoke <sniffjoke@gmail.com>
2024-07-29 11:34:07 +0200

    did reverse, join, split, concat, flat, find, includes, filter

--- index.js
+++ index.js
@@ -90,3 +90,67 @@
 // },)
 //
 // console.log(users)
+
+// const str = 'JavaScript is awesome'
+// console.log(str.split(' ').reverse().join(' '))
+
+// const mergeArrays = (arrOne, arrTwo) => {
+//     return arrOne.concat(arrTwo)
+// }
+// console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
+// console.log(mergeArrays(['apple', 'banana'], ['orange', 'kiwi']));
+//
+// const flattenArray = (arr) => {
+//     return arr.flat(Infinity)
+// }
+// console.log(flattenArray([1, [2, 3], [[4], [5, 6]]]))
+// console.log(flattenArray([1, [2, [3, [4, [5]]]]]))
+
+// const arr = [10, 20, 30, 40, 50]
+//
+// function great25 (arr) {
+//     return arr.find(x => x>25)
+// }
+//
+// console.log(great25(arr))
+//
+// const fruits = ["яблоко", "банан", "апельсин", "киви"]
+//
+// function startsWith (arr) {
+//     return arr.find(x => x[0] === 'а')
+// }
+//
+// console.log(startsWith(fruits))
+
+// const arr = [1, 2, 3, 4, 5];
+// function incle(arr) {
+//     return arr.includes(3)
+// }
+//
+// console.log(incle(arr))
+
+// const arr = ["яблоко", "банан", "апельсин", "киви"];
+// function incle(arr) {
+//     return arr.includes('Виноград')
+// }
+//
+// console.log(incle(arr))
+
+// const numbers = [1, 2, 3, 4, 5];
+// const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+//
+// function checkElement(arr, x) {
+//     return arr.includes(x)
+// }
+// console.log(checkElement(numbers, 3));
+// console.log(checkElement(fruits, 'grape'));
+
+// const arrOne = [10, 20, 30, 40, 50];
+// const arrTwo = [5, 15, 25, 35, 45];
+//
+// function filterGreaterThan (arr, x) {
+//     return arr.filter(it => it > x);
+// }
+//
+// console.log(filterGreaterThan(arrOne, 25))
+// console.log(filterGreaterThan(arrTwo, 20))
